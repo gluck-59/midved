4450b41a221355472bf1f7a7a9cf53a5f6176e47
gluck-59 <gluck59@gmail.com>
2022-09-18 20:42:28 +0500

    user-scalable=no

--- application/views/header.php
+++ application/views/header.php
@@ -6,7 +6,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1" user-scalable=no>
+	<meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
+
 	<link rel="icon" href="/application/favicon.ico">
 <!--	<link rel="canonical" href="https://getbootstrap.com/docs/3.3/examples/starter-template/">-->
 
