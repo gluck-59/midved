368c55b7e2b8bf3d63d3d82526ee2edb62fc42b7
gluck-59 <gluck59@gmail.com>
2022-09-25 12:33:36 +0500

    []

--- application/views/modals.php
+++ application/views/modals.php
@@ -139,11 +139,22 @@
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="glyphicon glyphicon-remove" style="zoom: 2;"></i></button>
 				<h3 class="modal-title" id="prihod_rashod"></h3>
-				<select id="requestList" data-live-search="true" title="В какую заявку?" data-width="150"></select>
-				<label for="paymentType" style="vertical-align: -webkit-baseline-middle;">
-					<input type="checkbox" id="paymentType" name="paymentType" class="1form-control" style="zoom: 1.3;">
-					работа
-				</label>
+				<div class="row">
+					<div class="col-xs-6">
+						<select id="requestList" data-live-search="true" title="В какую заявку?" data-width="150"></select>
+					</div>
+					<div class="col-xs-4">
+						<div class="checkbox">
+							<label>
+								<input type="checkbox" id="paymentType" name="paymentType" class="1form-control" style="/*zoom: 1.3;*/">
+								работа
+							</label>
+						</div>
+					</div>
+				</div>
+
+
+
 			</div>
 			<div class="modal-body">
 				<input type="number" pattern="[0-9]*" id="sum" class="form-control" style="zoom: 3;">
