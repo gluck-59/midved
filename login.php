ba2c88aaf9ac31732e72306b402311a4a9bf9bd2
gluck-59 <gluck59@gmail.com>
2022-09-27 19:29:57 +0500

    []

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
 	<link rel="icon" href="/application/favicon.ico">
+	<title>Войдите</title>
 
 	<script src="/application/js/jquery-1.9.1.js"></script>
 	<script src="/application/js/bootstrap.min.js"></script>
@@ -20,10 +21,10 @@
 	}
 
 	#pad	{
-		margin: 40vh auto;
-		/* width: 30vw; */
+		margin: 27vh auto;
+		width: 380px;
 		position: relative;
-		opacity: 0.8;
+		opacity: 0.9;
 	}
 	form {
 		border: solid 60px #f5eeeb;
@@ -31,9 +32,6 @@
 		bottom: -100px;
 		padding: 40px;
 	}
-
-	.form-control, button {
-		zoom: 1.6;}
 </style>
 
 <body>
@@ -42,17 +40,17 @@
 		<form method="post" action="/login" class="form-horizontal">
 			<div class="form-group">
 				<div class="col-sm-10">
-					<input type="text" class="form-control" name="user" placeholder="User">
+					<input type="text" class="form-control" name="user" placeholder="Имя">
 				</div>
 			</div>
 			<div class="form-group">
 				<div class="col-sm-10">
-					<input type="password" class="form-control" name="password" placeholder="Password">
+					<input type="password" class="form-control" name="password" placeholder="Пароль">
 				</div>
 			</div>
 			<div class="form-group">
 				<div class="col-sm-10">
-					<button type="submit" class="btn btn-success">Sign in</button>
+					<button type="submit" class="btn btn-success" style="width: 100%;">Войдите</button>
 				</div>
 			</div>
 		</form>
