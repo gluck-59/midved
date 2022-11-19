519629bd07a9564d67bab6711389fc3cbac933a8
Ruben Yakunchev <86118746+gluck-59@users.noreply.github.com>
2022-11-19 23:00:40 +0500

    customer верстка

--- application/views/customer.php
+++ application/views/customer.php
@@ -20,22 +20,25 @@
 		<div class="clearfix">&nbsp;</div>
 	</div>
 	<div class="row">
-		<div class="col-lg-6 col-xs-12">
+		<div class="col-lg-2 col-xs-12">
+			<table id="customers" class="table table-condensed 1table-bordered">
             <?php
                 foreach ($customers as $parent) { ?>
-                    <div>
-                        <b><a href="#" id="" data-toggle="modal" data-target="#modal-customer" data-customer_id="<?=$parent->id?>" data-name="<?=$parent->name?>" data-parent-id="<?=$parent->parentId?>" data-data="<?=$parent->data?>" ><?=$parent->name?></a>:</b>
-                        &nbsp;&nbsp;<button type="button" class="close deleteCustomer" id="<?=$parent->id?>"><i class="glyphicon glyphicon-remove"></i></button>
-                    </div>
+                    <tr>
+                        <td>
+                            <b><a href="#" id="" data-toggle="modal" data-target="#modal-customer" data-customer_id="<?=$parent->id?>" data-name="<?=$parent->name?>" data-parent-id="<?=$parent->parentId?>" data-data="<?=$parent->data?>" ><?=$parent->name?></a>:</b>
+                            &nbsp;&nbsp;<button type="button" class="close deleteCustomer" id="<?=$parent->id?>"><i class="glyphicon glyphicon-remove"></i></button>
                         <?php //if ($parent->childs)
                         {
                             foreach ($parent->childs as $child) {?>
-                        <div>
-                            &nbsp;&nbsp;- <a href="#" id="" data-toggle="modal" data-target="#modal-customer" data-customer_id="<?=$child->id?>" data-name="<?=$child->name?>" data-parent-id="<?=$child->parentId?>" data-data="<?=$child->data?>" ><?=$child->name?></a>
-                            &nbsp;&nbsp;<button type="button" class="close deleteCustomer" id="<?=$child->id?>"><i class="glyphicon glyphicon-remove"></i></button>
-                        </div>
-                        <?php }}} ?>
-		</div><br>
+                            <br>&nbsp;&nbsp;- <a href="#" id="" data-toggle="modal" data-target="#modal-customer" data-customer_id="<?=$child->id?>" data-name="<?=$child->name?>" data-parent-id="<?=$child->parentId?>" data-data="<?=$child->data?>" ><?=$child->name?></a>
+                                &nbsp;&nbsp;<button type="button" class="close deleteCustomer" id="<?=$parent->id?>"><i class="glyphicon glyphicon-remove"></i></button>
+                        <?php } ?>
+                        </td>
+                <?php }} ?>
+                    </tr>
+            </table>
+		</div>
 	</div>
 </div>
 
